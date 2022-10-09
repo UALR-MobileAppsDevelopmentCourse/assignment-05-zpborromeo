4e320241921dbda820edcbd19052a379800c3f58
zpborromeo <zborromeo@ualr.edu>
2022-10-09 12:30:30 -0500

    final commit - implementation complete

--- app/src/main/java/com/ualr/layoutassignment/MainActivity.java
+++ app/src/main/java/com/ualr/layoutassignment/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +19,37 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ToggleButton discountBtn = (ToggleButton) findViewById(R.id.discount_togglebtn);
+        ToggleButton noDiscountBtn = (ToggleButton) findViewById(R.id.noDiscount_togglebtn);
+
+        discountBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    discountBtn.setChecked(true);
+                    discountBtn.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                    discountBtn.setTextColor(getResources().getColor(R.color.grey_3));
+                    noDiscountBtn.setBackgroundColor(getResources().getColor(R.color.grey_3));
+                    noDiscountBtn.setTextColor(getResources().getColor(R.color.colorPrimary));
+                    noDiscountBtn.setChecked(false);
+                }
+
+            }
+        });
+
+        noDiscountBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    noDiscountBtn.setChecked(true);
+                    noDiscountBtn.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                    noDiscountBtn.setTextColor(getResources().getColor(R.color.grey_3));
+                    discountBtn.setBackgroundColor(getResources().getColor(R.color.grey_3));
+                    discountBtn.setTextColor(getResources().getColor(R.color.colorPrimary));
+                    discountBtn.setChecked(false);
+                }
+            }
+        });
     }
 
     public void calculateTotal(View view){
@@ -25,18 +58,21 @@ public class MainActivity extends AppCompatActivity {
         CheckBox product3CB = (CheckBox) findViewById(R.id.product3_cb);
         CheckBox product4CB = (CheckBox) findViewById(R.id.product4_cb);
 
+        ToggleButton discount = (ToggleButton) findViewById(R.id.discount_togglebtn);
+
         boolean p1Checked = product1CB.isChecked();
         boolean p2Checked = product2CB.isChecked();
         boolean p3Checked = product3CB.isChecked();
         boolean p4Checked = product4CB.isChecked();
+
+        boolean discountChecked = discount.isChecked();
 
         EditText p1Value = (EditText) findViewById(R.id.product1Value);
         EditText p2Value = (EditText) findViewById(R.id.product2Value);
         EditText p3Value = (EditText) findViewById(R.id.product3Value);
         EditText p4Value = (EditText) findViewById(R.id.product4Value);
 
-
-        double p1Val = 0, p2Val = 0, p3Val = 0, p4Val = 0;
+        double p1Val = 0.00, p2Val = 0.00, p3Val = 0.00, p4Val = 0.00, total;
 
         if(p1Checked){
             p1Val = Double.parseDouble(p1Value.getText().toString());
@@ -51,8 +87,13 @@ public class MainActivity extends AppCompatActivity {
             p4Val = Double.parseDouble(p4Value.getText().toString());
         }
 
-        double total = p1Val + p2Val + p3Val + p4Val;
-        String totalStr = Double.toString(total);
+        if(discountChecked){
+            total = (p1Val + p2Val + p3Val + p4Val) * 0.75;
+        }else{
+            total = p1Val + p2Val + p3Val + p4Val;
+        }
+
+        String totalStr = String.format("%.2f", total);
 
         TextView _total = (TextView) findViewById(R.id.total_dollars);
 
